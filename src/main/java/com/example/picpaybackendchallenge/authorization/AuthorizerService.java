3d9db94ee7f69a1b07c919ba92175443b9ab6658
Eriklps <erikdslopes@gmail.com>
2024-03-26 10:21:53 -0300

    implemented logging

--- src/main/java/com/example/picpaybackendchallenge/authorization/AuthorizerService.java
+++ src/main/java/com/example/picpaybackendchallenge/authorization/AuthorizerService.java
@@ -2,11 +2,14 @@ package com.example.picpaybackendchallenge.authorization;
 
 import com.example.picpaybackendchallenge.transaction.Transaction;
 import com.example.picpaybackendchallenge.transaction.UnauthorizedTransactionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 @Service
 public class AuthorizerService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorizerService.class);
     private RestClient restClient;
 
     public AuthorizerService(RestClient.Builder builder) {
@@ -16,12 +19,16 @@ public class AuthorizerService {
     }
 
     public void authorize(Transaction transaction) {
+        LOGGER.info("authorizing transaction {}...", transaction);
+
         var response = restClient.get()
                 .retrieve()
                 .toEntity(Authorization.class);
 
         if (response.getStatusCode().isError() || !response.getBody().isAuthorized())
             throw new UnauthorizedTransactionException("Unauthorized!");
+
+        LOGGER.info("Transaction authorized: {}", transaction);
     }
 
 }
--- src/main/java/com/example/picpaybackendchallenge/notification/NotificationConsumer.java
+++ src/main/java/com/example/picpaybackendchallenge/notification/NotificationConsumer.java
@@ -1,16 +1,19 @@
 package com.example.picpaybackendchallenge.notification;
 
+import com.example.picpaybackendchallenge.authorization.AuthorizerService;
 import com.example.picpaybackendchallenge.transaction.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 @Service
 public class NotificationConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorizerService.class);
     private RestClient restClient;
 
     public NotificationConsumer(RestClient.Builder builder) {
-
         this.restClient = builder.baseUrl(
                         "https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6")
                 .build();
@@ -18,12 +21,13 @@ public class NotificationConsumer {
 
     @KafkaListener(topics = "transaction-notification", groupId = "picpay-backend-challenge")
     public void receiveNotification(Transaction transaction) {
+        LOGGER.info("notifying transaction {}...", transaction);
 
-        var response = restClient.get()
-                .retrieve()
-                .toEntity(Notification.class);
+        var response = restClient.get().retrieve().toEntity(Notification.class);
 
         if (response.getStatusCode().isError() || !response.getBody().message())
             throw new NotificationException("Error notifying transaction " + transaction);
+
+        LOGGER.info("notification has been sent {}...", response.getBody());
     }
 }
