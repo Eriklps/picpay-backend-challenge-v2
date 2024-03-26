16edcbc178799ccf490ea75e3ad432f896df783c
Eriklps <erikdslopes@gmail.com>
2024-03-26 10:17:09 -0300

    implemented exception handler and added kafka topic to main

--- src/main/java/com/example/picpaybackendchallenge/PicpayBackendChallengeApplication.java
+++ src/main/java/com/example/picpaybackendchallenge/PicpayBackendChallengeApplication.java
@@ -1,7 +1,10 @@
 package com.example.picpaybackendchallenge;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class PicpayBackendChallengeApplication {
@@ -10,4 +13,9 @@ public class PicpayBackendChallengeApplication {
 		SpringApplication.run(PicpayBackendChallengeApplication.class, args);
 	}
 
+	@Bean
+	NewTopic notificationTopic() {
+		return TopicBuilder.name("transaction-notification")
+				.build();
+	}
 }
